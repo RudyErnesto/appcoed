ec8e28740856eb8729a2766e0292c4eeb99b3263
RudyErnesto <32149729+RudyErnesto@users.noreply.github.com>
2018-04-04 10:39:29 -0400

    primer comit
    subiendo la app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Proveedor1Provider } from '../../providers/proveedor1/proveedor1';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  mapas
+  constructor(public navCtrl: NavController, public proveedor:Proveedor1Provider) {
 
-  constructor(public navCtrl: NavController) {
-
+  }
+  ionViewDidLoad(){
+    this.proveedor.obtenerDatos()
+    .subscribe(
+      (data)=> {this.mapas = data;},
+      (error)=>{console.log(error);}
+  )
   }
 
 }
